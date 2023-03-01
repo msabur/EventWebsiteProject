a37042e3e13fe8488c7a2f68c43233ae6efed5b2
msabur <58122827+msabur@users.noreply.github.com>
2023-03-01 16:22:07 -0500

    add convenience wrapper around fetch()

---
+++ frontend/src/api/index.js
@@ -0,0 +1,51 @@
+import { AppState } from "../state/AppState";
+
+// adapted from https://jasonwatmore.com/post/2021/09/17/react-fetch-set-authorization-header-for-api-requests-if-user-logged-in
+export function useFetchWrapper() {
+    return {
+        get: request('GET'),
+        post: request('POST'),
+        put: request('PUT'),
+        delete: request('DELETE')
+    };
+
+    function request(method) {
+        return async (path, body) => {
+            const url = `http://localhost:3000${path}`;
+            const requestOptions = {
+                method,
+                headers: authHeader()
+            };
+            if (body) {
+                requestOptions.headers['Content-Type'] = 'application/json';
+                requestOptions.body = JSON.stringify(body);
+            }
+            const response = await fetch(url, requestOptions);
+            return handleResponse(response);
+        }
+    }
+
+    // helper functions
+
+    function authHeader() {
+        // return auth header with jwt if user is logged in
+        const isLoggedIn = AppState.loggedIn;
+        const token = AppState.token;
+        if (isLoggedIn) {
+            return { Authorization: `Bearer ${token}` };
+        } else {
+            return {};
+        }
+    }
+
+    function handleResponse(response) {
+        return response.json().then(data => {
+            if (!response.ok) {
+                const error = (data && data.message) || response.statusText;
+                AppState.onError(error);
+                return Promise.reject(error);
+            }
+            return data;
+        });
+    }
+}
