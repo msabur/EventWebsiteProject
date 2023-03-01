da45df2c915f0cfa9048dbae961c53accc86be0d
msabur <58122827+msabur@users.noreply.github.com>
2023-02-28 21:04:30 -0500

    Refactor repeated sql queries

---
+++ backend/db_helpers.js
@@ -0,0 +1,104 @@
+/**
+ * 
+ * @param {import('fastify').FastifyInstance} fastify 
+ * @param {number} id 
+ * @returns {Promise<boolean>}
+ */
+export async function checkIsSuperAdmin(fastify, id) {
+    const result = await fastify.pg.query('SELECT * FROM super_admins WHERE id = $1', [id])
+    return result.rows.length !== 0
+}
+
+/**
+ * @param {import('fastify').FastifyInstance} fastify
+ * @param {number} userId the id of the user
+ * @returns 
+ */
+export async function getEvents(fastify, userId) {
+    let query = (
+        `SELECT id, host_university_id, host_rso_id, event_name, description, location_name, time
+            FROM events
+            LEFT JOIN public_events USING (id)
+            LEFT JOIN private_events USING (id)
+            LEFT JOIN rso_events USING (id)
+        `)
+
+    let eventResult
+
+    const isSuperAdmin = await checkIsSuperAdmin(fastify, userId)
+    if (isSuperAdmin) {
+        console.log('isSuperAdmin', isSuperAdmin)
+        eventResult = await fastify.pg.query(query)
+    } else {
+        query += (`
+            WHERE (
+                public_events.id IS NOT NULL OR (
+                private_events.id IS NOT NULL AND
+                    private_events.host_university_id = (
+                        SELECT university_id
+                        FROM students
+                        WHERE students.id = $1
+                    )
+                ) OR (
+                    rso_events.id IS NOT NULL AND
+                    rso_events.host_rso_id IN (
+                        SELECT rso_id
+                        FROM rso_memberships
+                        WHERE rso_memberships.student_id = $1
+                    )
+                )
+            )
+        `)
+        eventResult = await fastify.pg.query(query, [userId])
+    }
+
+    return eventResult.rows || []
+}
+
+/**
+ * @param {import('fastify').FastifyInstance} fastify
+ * @param {number} eventId the id of the event
+ * @param {number} userId the id of the user
+ * @returns {Promise<Object>}
+ */
+
+export async function getEvent(fastify, eventId, userId) {
+    let query = (
+        `SELECT id, host_university_id, host_rso_id, email_address, category, phone_number, description, time, event_name, location_name, location_latitude, location_longitude
+            FROM events
+            LEFT JOIN public_events USING (id)
+            LEFT JOIN private_events USING (id)
+            LEFT JOIN rso_events USING (id)
+            WHERE events.id = $1
+        `)
+
+    let eventResult
+
+    const isSuperAdmin = await checkIsSuperAdmin(fastify, userId)
+    if (isSuperAdmin) {
+        eventResult = await fastify.pg.query(query, [eventId])
+    } else {
+        query += (`
+            AND (
+                public_events.id IS NOT NULL OR (
+                private_events.id IS NOT NULL AND
+                    private_events.host_university_id = (
+                        SELECT university_id
+                        FROM students
+                        WHERE students.id = $2
+                    )
+                ) OR (
+                    rso_events.id IS NOT NULL AND
+                    rso_events.host_rso_id IN (
+                        SELECT rso_id
+                        FROM rso_memberships
+                        WHERE rso_memberships.student_id = $2
+                    )
+                )
+            )
+        `)
+        eventResult = await fastify.pg.query(query, [eventId, userId])
+    }
+
+    return eventResult.rows[0] || null
+}
--- backend/routes/event-routes.js
+++ backend/routes/event-routes.js
@@ -1,8 +1,12 @@
+import { checkIsSuperAdmin } from "../db_helpers.js"
+import { getEvents, getEvent } from "../db_helpers.js"
+
 /**
  * A plugin that provide encapsulated routes
  * @param {FastifyInstance} fastify encapsulated fastify instance
  * @param {Object} options plugin options, refer to https://www.fastify.io/docs/latest/Reference/Plugins/#plugin-options
  */
+
 async function routes(fastify, options) {
     // GET /events: get basic info of all events
     // GET /events/:id: get full info of an event and its feedbacks (comments/ratings)
@@ -11,75 +15,7 @@ async function routes(fastify, options) {
 
     fastify.get('/events', { preHandler: fastify.authenticate, }, async (request, reply) => {
         const { id } = request.user
-        let publicEventsResult, privateEventsResult, rsoEventsResult
-        // if superadmin, return all events
-        // otherwise, return:
-        // public events, private events at his university,
-        // RSO events at RSOs he is in
-
-        const superAdminResult = await fastify.pg.query('SELECT * FROM super_admins WHERE id = $1', [id])
-        const isSuperAdmin = superAdminResult.rows.length != 0
-        if (isSuperAdmin) {
-            publicEventsResult = await fastify.pg.query(
-                `SELECT public_events.id, event_name, description, location_name, time
-                FROM public_events
-                JOIN events ON public_events.id = events.id`
-            )
-            privateEventsResult = await fastify.pg.query(
-                `SELECT private_events.id, event_name, description, location_name, time
-                FROM private_events
-                JOIN events ON private_events.id = events.id`
-            )
-            rsoEventsResult = await fastify.pg.query(
-                `SELECT rso_events.id, event_name, description, location_name, time
-                FROM rso_events
-                JOIN events ON rso_events.id = events.id`
-            )
-        } else {
-            publicEventsResult = await fastify.pg.query(
-                `SELECT public_events.id, event_name, description, location_name, time
-                FROM public_events
-                JOIN events ON public_events.id = events.id`
-            )
-
-            privateEventsResult = await fastify.pg.query(
-                `SELECT private_events.id, event_name, description, location_name, time
-                FROM private_events
-                JOIN events ON private_events.id = events.id
-                WHERE private_events.host_university_id = (
-                    SELECT university_id
-                    FROM students
-                    WHERE students.id = $1
-                )`,
-                [id]
-            )
-
-            rsoEventsResult = await fastify.pg.query(
-                `SELECT rso_events.id, event_name, description, location_name, time
-                FROM rso_events
-                JOIN events ON rso_events.id = events.id
-                WHERE rso_events.host_rso_id IN (
-                    SELECT rso_id
-                    FROM rso_memberships
-                    WHERE rso_memberships.student_id = $1
-                )`,
-                [id]
-            )
-
-        }
-
-        // add a field with event type
-        const publicEvents = publicEventsResult.rows.map(event => (
-            { ...event, type: 'public' }
-        ))
-        const privateEvents = privateEventsResult.rows.map(event => (
-            { ...event, type: 'private' }
-        ))
-        const rsoEvents = rsoEventsResult.rows.map(event => (
-            { ...event, type: 'rso' }
-        ))
-
-        const events = [...publicEvents, ...privateEvents, ...rsoEvents]
+        const events = await getEvents(fastify, id)
         reply.send({ events })
     })
 
@@ -95,102 +31,22 @@ async function routes(fastify, options) {
             }
         }
     }, async (request, reply) => {
-        const { id } = request.params
+        const { id: eventId } = request.params
         const { id: userId } = request.user
-        let publicEventsResult, privateEventsResult, rsoEventsResult
-        let feedbacksResult
 
-        const superAdminResult = await fastify.pg.query('SELECT * FROM super_admins WHERE id = $1', [userId])
-        const isSuperAdmin = superAdminResult.rows.length != 0
-        if (isSuperAdmin) {
-            publicEventsResult = await fastify.pg.query(
-                `SELECT public_events.id, email_address, category, phone_number, description, time, event_name, location_name, location_latitude, location_longitude
-                FROM public_events
-                JOIN events ON public_events.id = events.id
-                WHERE public_events.id = $1`,
-                [id]
-            )
-            privateEventsResult = await fastify.pg.query(
-                `SELECT private_events.id, private_events.host_university_id, email_address, category, phone_number, description, time, event_name, location_name, location_latitude, location_longitude
-                FROM private_events
-                JOIN events ON private_events.id = events.id
-                WHERE private_events.id = $1`,
-                [id]
-            )
-            rsoEventsResult = await fastify.pg.query(
-                `SELECT rso_events.id, rso_events.host_rso_id, email_address, category, phone_number, description, time, event_name, location_name, location_latitude, location_longitude
-                FROM rso_events
-                JOIN events ON rso_events.id = events.id
-                WHERE rso_events.id = $1`,
-                [id]
-            )
-        } else {
-            publicEventsResult = await fastify.pg.query(
-                `SELECT public_events.id, email_address, category, phone_number, description, time, event_name, location_name, location_latitude, location_longitude
-                FROM public_events
-                JOIN events ON public_events.id = events.id
-                WHERE public_events.id = $1`,
-                [id]
-            )
-            privateEventsResult = await fastify.pg.query(
-                `SELECT private_events.id, private_events.host_university_id, email_address, category, phone_number, description, time, event_name, location_name, location_latitude, location_longitude
-                FROM private_events
-                JOIN events ON private_events.id = events.id
-                WHERE private_events.id = $1 AND
-                    private_events.host_university_id = (
-                        SELECT university_id
-                        FROM students
-                        WHERE students.id = $2
-                )`,
-                [id, userId]
-            )
-            rsoEventsResult = await fastify.pg.query(
-                `SELECT rso_events.id, rso_events.host_rso_id, email_address, category, phone_number, description, time, event_name, location_name, location_latitude, location_longitude
-                FROM rso_events
-                JOIN events ON rso_events.id = events.id
-                WHERE rso_events.id = $1 AND
-                    rso_events.host_rso_id IN (
-                        SELECT rso_id
-                        FROM rso_memberships
-                        WHERE rso_memberships.student_id = $2
-                )`,
-                [id, userId]
-            )
-        }
-
-        feedbacksResult = await fastify.pg.query(
-            `SELECT feedbacks.id, feedbacks.comment, feedbacks.rating, users.username
-            FROM feedbacks
-            JOIN users ON feedbacks.author_id = users.id
-            WHERE feedbacks.event_id = $1`,
-            [id]
-        )
-
-        const feedbacks = feedbacksResult.rows
-
-        const publicEvent = publicEventsResult.rows[0]
-        const privateEvent = privateEventsResult.rows[0]
-        const rsoEvent = rsoEventsResult.rows[0]
-
-        if (publicEvent) {
-            reply.send({
-                event: { ...publicEvent, type: 'public' },
-                feedbacks
-            })
-        } else if (privateEvent) {
-            reply.send({
-                event: {
-                    ...privateEvent, type: 'private'
-                },
-                feedbacks
-            })
-        } else if (rsoEvent) {
-            reply.send({
-                event: { ...rsoEvent, type: 'rso' },
-                feedbacks
-            })
-        } else {
+        const event = await getEvent(fastify, eventId, userId)
+        if (!event) {
             reply.code(404).send({ message: 'Event not found' })
+        } else {
+            const feedbacksResult = await fastify.pg.query(
+                `SELECT users.username, comment, rating
+                FROM feedbacks
+                JOIN users ON feedbacks.author_id = users.id
+                WHERE event_id = $1`,
+                [eventId]
+            )
+            const feedbacks = feedbacksResult.rows || []
+            reply.send({ event, feedbacks })
         }
     })
 
@@ -219,47 +75,13 @@ async function routes(fastify, options) {
             }
         }
     }, async (request, reply) => {
-        const { id } = request.params
+        const { id: eventId } = request.params
         const { id: userId } = request.user
         const { comment, rating } = request.body
 
         // check if user can access this event
-        let eventFound = false
-
-        const isSuperAdmin = (await fastify.pg.query('SELECT * FROM super_admins WHERE id = $1', [userId])).rows.length != 0
-        if (isSuperAdmin) {
-            const eventResult = await fastify.pg.query(
-                `SELECT * FROM events WHERE id = $1`,
-                [id]
-            )
-            eventFound = eventResult.rows.length != 0
-        } else {
-            const publicEventResult = await fastify.pg.query(
-                `SELECT public_events.id FROM public_events WHERE id = $1`,
-                [id]
-            )
-            const privateEventResult = await fastify.pg.query(
-                `SELECT private_events.id FROM private_events WHERE id = $1 AND
-                    private_events.host_university_id = (
-                        SELECT university_id
-                        FROM students
-                        WHERE students.id = $2
-                    )`,
-                [id, userId]
-            )
-            const rsoEventResult = await fastify.pg.query(
-                `SELECT rso_events.id FROM rso_events WHERE id = $1 AND
-                    rso_events.host_rso_id IN (
-                        SELECT rso_id
-                        FROM rso_memberships
-                        WHERE rso_memberships.student_id = $2
-                    )`,
-                [id, userId]
-            )
-            eventFound = publicEventResult.rows.length != 0 || privateEventResult.rows.length != 0 || rsoEventResult.rows.length != 0
-        }
-
-        if (!eventFound) {
+        const event = await getEvent(fastify, eventId, userId)
+        if (!event) {
             reply.code(404).send({ message: 'Event not found' })
             return
         }
